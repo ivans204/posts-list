ed026aead355613d8af7c1c5e958bde588af51c2
ivans204 <ivansvrga204@gmail.com>
2021-04-10 23:38:42 +0200

    show comments for posts

--- src/pages/PostList/index.js
+++ src/pages/PostList/index.js
@@ -1,27 +1,42 @@
 import { useEffect, useState } from 'react';
 
-import { getPosts } from '../../api';
+import { getPosts, getComments } from '../../api';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     getPosts().then((posts) => {
       setPosts(posts.data);
     });
+
+    getComments().then((comments) => {
+      setComments(comments.data);
+    });
   }, []);
 
   return (
     <div>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <p>title: {post.title}</p>
-            <p>body: {post.body}</p>
-            <hr />
-          </div>
-        );
-      })}
+      {posts &&
+        // eslint-disable-next-line
+        posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <p>title: {post.title}</p>
+              <p>body: {post.body}</p>
+              <p>Comments:</p>
+              <ul>
+                {comments.map((comment) => {
+                  if (comment.postId === post.id) {
+                    return <li key={comment.id}>{comment.body}</li>;
+                  }
+                })}
+              </ul>
+              <hr />
+            </div>
+          );
+        })}
     </div>
   );
 };
