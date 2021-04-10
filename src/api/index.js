88dd6749f192bf56f465f05579e74467d8c312aa
ivans204 <ivansvrga204@gmail.com>
2021-04-11 01:20:37 +0200

    api get comments by post id

--- src/api/index.js
+++ src/api/index.js
@@ -17,3 +17,7 @@ export const getUsers = async () => {
 export const getPostById = async (postId) => {
   return await axios.get(`${baseUrl}/posts/${postId}`);
 };
+
+export const getCommentsByPostId = async (postId) => {
+  return await axios.get(`${baseUrl}/comments?postId=${postId}`);
+};
