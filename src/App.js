070e7711035b554d4bf164904a1f038d2af4ee52
ivans204 <ivansvrga204@gmail.com>
2021-04-11 00:16:32 +0200

    created propMessage and passing it to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import PostList from './pages/PostList';
 
+const propMessage = 'Hello from';
+
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <PostList propMessage={propMessage} />
     </div>
   );
 }
